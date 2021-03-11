6253985f547b4ebd65be146ce2eb7d9e61f0789a
Logan_Sel <logansel59162@gmail.com>
2021-03-11 18:08:11 +0100

    exo 1

---
+++ 02_programming/01_algorithms_day_1/c2fee3d27668042d64eb9d05e174fd5e9db87123/01_play_with_variables/src/01_play_with_variables.js
@@ -0,0 +1,17 @@
+// a constant called `question`, bound to a string: "Spartan, what is your profession?!";
+const question = "Spartan";
+// a constant called `answer`, bound to a string: "Dev dev dev!";
+const answer = "Dev dev dev!";
+// a constant called `foo`, bound to an integer: 12;
+const foo = 12;
+// a constant called `bar`, bound to an integer: 28;
+const bar = 28;
+// a constant called `sumResult`, bound to the sum of `foo` and `bar`;
+const sumResult = foo + bar;
+// a constant called `prodResult`, bound to the product of `bar` and `foo`.
+const prodResult = foo * bar;
+// a constant called `sparta`, bound to an object with a field `check`, with the value of `ask` and with the field `response`, with the value of `answer`;
+
+// a constant called `leader`, bound to the value of the field `check` of the object `sparta`;
+
+// a constant called `digits`, bound to an array from zero to nine.
