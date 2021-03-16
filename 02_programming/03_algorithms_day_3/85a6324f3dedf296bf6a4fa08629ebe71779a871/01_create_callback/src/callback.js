dc8f0ad35b295811a06731f041083e5d6e714cfa
Logan_Sel <logansel59162@gmail.com>
2021-03-16 10:49:25 +0100

    exo lundi 15

---
+++ 02_programming/03_algorithms_day_3/85a6324f3dedf296bf6a4fa08629ebe71779a871/01_create_callback/src/callback.js
@@ -0,0 +1,7 @@
+// code here
+
+
+const sayHello = (player) => console.log("Hello, Spartacus");
+
+// leave the code below for tests to work
+module.exports = sayHello;
---
+++ 02_programming/03_algorithms_day_3/85a6324f3dedf296bf6a4fa08629ebe71779a871/02_use_callback/src/getInput.js
@@ -0,0 +1,9 @@
+// A function that takes two parameters, the second one being a callback function
+
+const getInput = (input, callback) => callback(input.key);
+
+
+
+// Leave the line below for tests to work
+module.exports = getInput;
+
