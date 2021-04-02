7b37fe2025ae7d278dbee672cbf76a558ce15874
Logan_Sel <logansel59162@gmail.com>
2021-04-02 10:53:06 +0200

    1avril

---
+++ 03_backend_web_development_and_api/02_HTTP/77d581a2e2053b435f7882811ebedc634cb80606/03_chuck_norris/src/getCategories.ts
@@ -0,0 +1,8 @@
+import fetch from "node-fetch"
+
+function getCategories() {
+  // code the function here
+}
+
+// leave line below for tests to work properly
+export { getCategories }
---
+++ 03_backend_web_development_and_api/02_HTTP/77d581a2e2053b435f7882811ebedc634cb80606/03_chuck_norris/src/getChuckNorrisJoke.ts
@@ -0,0 +1,8 @@
+import fetch from "node-fetch"
+
+function getChuckNorrisJoke() {
+  // code the function here
+}
+
+// leave line below for tests to work properly
+export { getChuckNorrisJoke }
